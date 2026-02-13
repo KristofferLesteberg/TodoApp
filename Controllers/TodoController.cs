7d5541d40ee10bc6374ea1839ce561a2da4e8fb3
unknown <edu6194294@agderskolen.no>
2026-02-13 09:52:47 +0100

    Lagt til README.md fil

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TodoApp.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
     public class TodoAppController : ControllerBase
--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -6,6 +6,8 @@ namespace PostgresAPI.Models
 
     public string? Name { get; set; }
 
+    public string? Description { get; set; }
+
     public bool IsComplete { get; set; }
 
 }
